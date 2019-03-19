4b30280dbda6dc79463658b0a725968f7f0e3f7a
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-19 13:34:03 +0100

    Make marketplace database urls configurable

--- marketplace/src/hermes/config.py
+++ marketplace/src/hermes/config.py
@@ -5,10 +5,13 @@ API_TOKEN_DURATION = timedelta(days=30)
 PASSWORD_RESET_TOKEN_DURATION = timedelta(days=1)
 EMAIL_VERIFICATION_TOKEN_DURATION = timedelta(days=3)
 
-SQL_ALCHEMY_SQLITE = 'sqlite:///dev.db'
-SQL_ALCHEMY_POSTGRESQL = 'postgresql://{username}:{password}@{db_host}:{db_port}/{db_name}'
-SQL_ALCHEMY_POSTGRESQL_PSYCOPG = 'postgresql+psycopg2://{username}:{password}@{db_host}:{db_port}/{db_name}'
+SQL_ALCHEMY_SQLITE_TEMPLATE = 'sqlite://{file_path}'
+SQL_ALCHEMY_SQLITE_ABS_PATH_TEMPLATE = 'sqlite:///{abs_file_path}'
+SQL_ALCHEMY_POSTGRESQL_TEMPLATE = 'postgresql://{username}:{password}@{db_host}:{db_port}/{db_name}'
+SQL_ALCHEMY_POSTGRESQL_PSYCOPG_TEMPLATE = 'postgresql+psycopg2://{username}:{password}@{db_host}:{db_port}/{db_name}'
 
 SALT = '2sTOkalOpHhHuNmod5GrGgijXkmkUsJz'
+
+DATABASE = SQL_ALCHEMY_SQLITE_TEMPLATE.format(file_path='dev.db')
 
 DEBUG_DB = False
