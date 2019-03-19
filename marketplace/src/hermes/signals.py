02dc961b38885507e5022403351ec388bc538302
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-19 13:44:32 +0100

    Create thread local db session in marketplace

--- marketplace/src/hermes/signals.py
+++ marketplace/src/hermes/signals.py
@@ -1,11 +1,18 @@
-from flask import Flask, session
+from flask import Flask, g
 from flask.wrappers import Response
+
+from hermes.db import init_db
 
 
 def register_signals(app: Flask):
+    @app.before_request
+    def before_request(*args, **kwargs):
+        init_db()
 
     @app.after_request
     def after_request(response: Response):
         """Automatically commit the User session object changes once a request is finished"""
-        session.db_session.commit()
+        g.db_session.commit()
+        # Once the commit is finished then cleanup the session object itself
+        g.db_session.remove()
         return response
