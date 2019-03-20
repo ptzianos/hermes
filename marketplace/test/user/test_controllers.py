52fc5b6bdec8d6b9c3450f3e6bca9c38e5be70fb
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-20 17:23:36 +0100

    Add test for resolve_token controller helper

--- marketplace/test/user/test_controllers.py
+++ marketplace/test/user/test_controllers.py
@@ -1,8 +1,9 @@
-from typing import Iterator, Tuple
+from typing import Callable, Iterator, Tuple
 
+import pytest
 from flask import Flask
 
-from hermes.types import EmailAddressType, UserType
+from hermes.types import EmailAddressType, SessionTokenType, UserType
 
 
 def test_resolve_user_helper(
@@ -28,3 +29,30 @@ def test_resolve_user_helper(
         assert_user(resolve_user(test_user), test_user)
         assert_user(resolve_user(test_user.name), test_user)
         assert_user(resolve_user(test_user.uuid), test_user)
+        assert_user(resolve_user(test_email.address), test_user)
+        with pytest.raises(Exception):
+            resolve_user(1)
+        assert resolve_user('bla') is None
+
+
+def test_resolve_token_helper(
+        flask_app: Flask,
+        user_generator: Iterator[UserType],
+        user_session_factory: Callable[[UserType], SessionTokenType]
+) -> None:
+    with flask_app.app_context():
+        from flask import g
+        from hermes.user.controllers import resolve_token
+
+        test_user, _ = next(user_generator)
+        test_session = user_session_factory(test_user)
+        # Setup user and email
+        g.db_session = flask_app.new_db_session_instance()
+        g.db_session.add(test_user)
+        g.db_session.add(test_session)
+        g.db_session.commit()
+
+        assert resolve_token(test_session).id == test_session.id, 'Wrong token resolved'
+        assert resolve_token(test_session.token).id == test_session.id, 'Wrong token resolved'
+        with pytest.raises(Exception):
+            resolve_token(1)
