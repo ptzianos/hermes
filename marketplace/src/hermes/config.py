2e068be2ee79018401eb3eff5483d834f1134634
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-20 23:08:43 +0100

    Add configuration values for public key verification workflow

--- marketplace/src/hermes/config.py
+++ marketplace/src/hermes/config.py
@@ -4,6 +4,7 @@ SESSION_DURATION = timedelta(days=1)
 API_TOKEN_DURATION = timedelta(days=30)
 PASSWORD_RESET_TOKEN_DURATION = timedelta(days=1)
 EMAIL_VERIFICATION_TOKEN_DURATION = timedelta(days=3)
+PUBLIC_KEY_VERIFICATION_REQUEST_DURATION = timedelta(days=1)
 
 SQL_ALCHEMY_SQLITE_TEMPLATE = 'sqlite://{file_path}'
 SQL_ALCHEMY_SQLITE_ABS_PATH_TEMPLATE = 'sqlite:///{abs_file_path}'
@@ -15,3 +16,11 @@ SALT = '2sTOkalOpHhHuNmod5GrGgijXkmkUsJz'
 DATABASE = SQL_ALCHEMY_SQLITE_TEMPLATE.format(file_path='dev.db')
 
 DEBUG_DB = False
+
+PUBLIC_KEY_VERIFICATION_TEXT = """
+By signing this text with your private key you verify that you are the sole rightful owner of the key pair whose
+public key has a SHA3-512 digest equal to {digest}.
+This makes you legally responsible for all actions performed on the platform and liable for any malicious
+activities that might occur.
+If you have not yourself requested this key to be verified, please do not sign this document.
+"""
