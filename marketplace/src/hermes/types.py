91d4709536b102da4fabbfbc47ba47412d93eeef
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-20 14:19:58 +0100

    Add types for EmailAddress and PublicKey models in market

--- marketplace/src/hermes/types.py
+++ marketplace/src/hermes/types.py
@@ -1,2 +1,4 @@
 UserType = 'hermes.user.models.User'
+EmailAddressType = 'hermes.user.models.EmailAddress'
+PublicKeyType = 'hermes.user.models.PublicKey'
 SessionTokenType = 'hermes.user.models.SessionToken'
