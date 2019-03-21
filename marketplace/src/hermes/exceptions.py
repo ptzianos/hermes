5be27225db17282fecbf16a6763a69b618054a53
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-21 18:18:06 +0100

    Add more exceptions for token generation and management in the market

--- marketplace/src/hermes/exceptions.py
+++ marketplace/src/hermes/exceptions.py
@@ -24,3 +24,23 @@ class AlreadyRegistered(Exception):
 
 class AlreadyVerified(Exception):
     msg = 'already_verified'
+
+
+class UnknownToken(Exception):
+    msg = 'unknown_token'
+
+
+class UserHasNoPassword(Exception):
+    msg = 'no_password'
+
+
+class WeakPassword(Exception):
+    msg = 'password_not_strong_enough'
+
+
+class NoPassword(Exception):
+    msg = 'no_password'
+
+
+class UnsupportedPublicKeyType(Exception):
+    msg = 'unsupported_public_key_type'
