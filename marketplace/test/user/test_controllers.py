013afd18912ed8433347b24d9af78a004b3dfad6
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-21 19:18:56 +0100

    Test generate_api controller

--- marketplace/test/user/test_controllers.py
+++ marketplace/test/user/test_controllers.py
@@ -3,6 +3,7 @@ from typing import Callable, Iterator, Tuple
 import pytest
 from flask import Flask
 
+from hermes.exceptions import UnknownToken, UnknownUser
 from hermes.types import EmailAddressType, SessionTokenType, UserType
 
 
@@ -54,5 +55,26 @@ def test_resolve_token_helper(
 
         assert resolve_token(test_session).id == test_session.id, 'Wrong token resolved'
         assert resolve_token(test_session.token).id == test_session.id, 'Wrong token resolved'
-        with pytest.raises(Exception):
+        with pytest.raises(UnknownToken):
             resolve_token(1)
+
+
+def test_generate_api_token(
+        flask_app: Flask,
+        user_generator: Iterator[Tuple[UserType, EmailAddressType]]):
+    with flask_app.app_context():
+        from flask import g
+        from hermes.user.controllers import generate_api_token
+
+        test_user, _ = next(user_generator)
+        # Setup user and email
+        g.db_session = flask_app.new_db_session_instance()
+        g.db_session.add(test_user)
+        g.db_session.commit()
+
+        new_token = generate_api_token(test_user)
+        assert new_token.token, 'Wrong token created'
+        assert not new_token.expired, 'Wrong token created'
+        assert not new_token.is_expired, 'Wrong token created'
+        with pytest.raises(UnknownUser):
+            generate_api_token('1')
