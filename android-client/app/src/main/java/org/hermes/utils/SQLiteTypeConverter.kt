3e97544684bd32d447fcf68a05c0f3c24205b38e
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-28 16:03:36 +0100

    Add helper for converting timestamps to ISO 8601 strings in SQLite

---
+++ android-client/app/src/main/java/org/hermes/utils/SQLiteTypeConverter.kt
@@ -0,0 +1,23 @@
+package org.hermes.utils
+
+import android.arch.persistence.room.TypeConverter
+import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.format.DateTimeFormatter
+
+object SQLiteTypeConverter {
+    private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+
+    @TypeConverter
+    @JvmStatic
+    fun toOffsetDateTime(value: String?): OffsetDateTime? {
+        return value?.let {
+            return formatter.parse(value, OffsetDateTime::from)
+        }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun fromOffsetDateTime(date: OffsetDateTime?): String? {
+        return date?.format(formatter)
+    }
+}
