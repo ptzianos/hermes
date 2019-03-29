6c77b1c4e261d424501c8147b7fe8e989347e122
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-29 11:40:11 +0100

    Return LiveData from queries of EventDao

--- android-client/app/src/main/java/org/hermes/daos/EventDao.kt
+++ android-client/app/src/main/java/org/hermes/daos/EventDao.kt
@@ -1,5 +1,6 @@
 package org.hermes.daos
 
+import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
@@ -10,37 +11,37 @@ import org.threeten.bp.OffsetDateTime
 @Dao
 interface EventDao {
     @Query("SELECT * FROM events ORDER BY datetime(created_on)")
-    fun getAll(): List<Event>
+    fun getAll(): LiveData<List<Event>>
 
     @Query("SELECT * FROM events WHERE uid == :eventId")
     fun findById(eventId: Int): Event
 
     @Query("SELECT * FROM events WHERE datetime(created_on) >= :createdAfter")
-    fun findAllAfterDate(createdAfter: OffsetDateTime)
+    fun findAllAfterDate(createdAfter: OffsetDateTime): LiveData<List<Event>>
 
     @Query("SELECT * FROM events WHERE datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore) ORDER BY datetime(created_on)")
-    fun findAllBetween(createdAfter: OffsetDateTime, createdBefore: OffsetDateTime)
+    fun findAllBetween(createdAfter: OffsetDateTime, createdBefore: OffsetDateTime): LiveData<List<Event>>
 
     @Query("SELECT * FROM events WHERE (datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore)) AND uid > :latestUId ORDER BY datetime(created_on) LIMIT :limit")
-    fun findAllBetweenDatesPaged(createdAfter: OffsetDateTime, createdBefore: OffsetDateTime, latestUId: Int = -1, limit: Int)
+    fun findAllBetweenDatesPaged(createdAfter: OffsetDateTime, createdBefore: OffsetDateTime, latestUId: Int = -1, limit: Int): LiveData<List<Event>>
 
     @Query("SELECT * FROM events WHERE resource = :resource")
-    fun findAllForResource(resource: String)
+    fun findAllForResource(resource: String): LiveData<List<Event>>
 
     @Query("SELECT * FROM events WHERE resource = :resource AND uid > :latestUId ORDER BY datetime(created_on) LIMIT :limit")
-    fun findAllForResourcePaged(resource: String, latestUId: Int = -1, limit: Int)
+    fun findAllForResourcePaged(resource: String, latestUId: Int = -1, limit: Int): LiveData<List<Event>>
 
     @Query("SELECT * FROM events WHERE (datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore)) AND resource = :resource ORDER BY datetime(created_on)")
     fun findAllBetweenDatesForResource(resource: String,
                                        createdAfter: OffsetDateTime,
-                                       createdBefore: OffsetDateTime)
+                                       createdBefore: OffsetDateTime): LiveData<List<Event>>
 
     @Query("SELECT * FROM events WHERE (datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore)) AND resource = :resource AND uid > :latestUId ORDER BY datetime(created_on) LIMIT :limit")
     fun findAllBetweenDatesForResourcePaged(resource: String,
                                             createdAfter: OffsetDateTime,
                                             createdBefore: OffsetDateTime,
                                             latestUId: Int = -1,
-                                            limit: Int)
+                                            limit: Int): LiveData<List<Event>>
 
     @Insert
     fun insertAll(vararg events: Event)
