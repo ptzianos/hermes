612968a0251181c8b6943bd880e059ec53bf5ad6
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-29 08:55:37 +0100

    Add toString method on Event entity

--- android-client/app/src/main/java/org/hermes/entities/Event.kt
+++ android-client/app/src/main/java/org/hermes/entities/Event.kt
@@ -4,7 +4,9 @@ import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import android.support.annotation.NonNull
+import org.hermes.utils.SQLiteTypeConverter
 import org.threeten.bp.OffsetDateTime
+import java.lang.StringBuilder
 
 @Entity(tableName = "events")
 data class Event(
@@ -14,4 +16,19 @@ data class Event(
     @ColumnInfo(name = "resource_id") var resourceId: Int?,
     @ColumnInfo(name = "extra_info") var extraInfo: String?,
     @NonNull @ColumnInfo(name = "created_on") val createdOn: OffsetDateTime? = null
-)
+) {
+    override fun toString(): String {
+        val safeTs = if (createdOn == null) "" else SQLiteTypeConverter.fromOffsetDateTime(createdOn)
+        return StringBuilder()
+                .append(safeTs)
+                .append(" : ")
+                .append(action)
+                .append(" : ")
+                .append(resource)
+                .append("::")
+                .append(resourceId ?: -1)
+                .append(" : ")
+                .append(extraInfo ?: "")
+                .toString()
+    }
+}
