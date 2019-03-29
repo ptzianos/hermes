5ea391a7aeda227601250b19ab475864fb863952
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-03-29 11:36:52 +0100

    Add paging to some queries of the EventDao

--- android-client/app/src/main/java/org/hermes/daos/EventDao.kt
+++ android-client/app/src/main/java/org/hermes/daos/EventDao.kt
@@ -21,13 +21,26 @@ interface EventDao {
     @Query("SELECT * FROM events WHERE datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore) ORDER BY datetime(created_on)")
     fun findAllBetween(createdAfter: OffsetDateTime, createdBefore: OffsetDateTime)
 
+    @Query("SELECT * FROM events WHERE (datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore)) AND uid > :latestUId ORDER BY datetime(created_on) LIMIT :limit")
+    fun findAllBetweenDatesPaged(createdAfter: OffsetDateTime, createdBefore: OffsetDateTime, latestUId: Int = -1, limit: Int)
+
     @Query("SELECT * FROM events WHERE resource = :resource")
     fun findAllForResource(resource: String)
+
+    @Query("SELECT * FROM events WHERE resource = :resource AND uid > :latestUId ORDER BY datetime(created_on) LIMIT :limit")
+    fun findAllForResourcePaged(resource: String, latestUId: Int = -1, limit: Int)
 
     @Query("SELECT * FROM events WHERE (datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore)) AND resource = :resource ORDER BY datetime(created_on)")
     fun findAllBetweenDatesForResource(resource: String,
                                        createdAfter: OffsetDateTime,
                                        createdBefore: OffsetDateTime)
+
+    @Query("SELECT * FROM events WHERE (datetime(created_on) BETWEEN datetime(:createdAfter) AND datetime(:createdBefore)) AND resource = :resource AND uid > :latestUId ORDER BY datetime(created_on) LIMIT :limit")
+    fun findAllBetweenDatesForResourcePaged(resource: String,
+                                            createdAfter: OffsetDateTime,
+                                            createdBefore: OffsetDateTime,
+                                            latestUId: Int = -1,
+                                            limit: Int)
 
     @Insert
     fun insertAll(vararg events: Event)
