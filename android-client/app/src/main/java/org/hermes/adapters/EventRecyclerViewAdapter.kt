a3b17bef4ca6e2ef80f641b76cd10c23021d55a2
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-04-03 11:05:48 +0200

    Cleanup EventRecyclerViewAdapter and add meethod setEvents for updating elements

--- android-client/app/src/main/java/org/hermes/adapters/EventRecyclerViewAdapter.kt
+++ android-client/app/src/main/java/org/hermes/adapters/EventRecyclerViewAdapter.kt
@@ -1,18 +1,18 @@
 package org.hermes.adapters
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.RelativeLayout
 import android.widget.TextView
+import java.util.*
 
 
 import org.hermes.R
 import org.hermes.entities.Event
-import org.hermes.fragments.EventFragment
 import org.hermes.fragments.EventFragment.OnListFragmentInteractionListener
 import org.hermes.utils.SQLiteTypeConverter
 
@@ -20,56 +20,46 @@ import org.hermes.utils.SQLiteTypeConverter
  * [RecyclerView.Adapter] that can display a [Event] and makes a call to the
  * specified [OnListFragmentInteractionListener].
  */
-class EventRecyclerViewAdapter(
-        private val mValues: List<Event>,
-        private val mListener: EventFragment.OnListFragmentInteractionListener?)
-    : RecyclerView.Adapter<EventRecyclerViewAdapter.ViewHolder>() {
-
+class EventRecyclerViewAdapter(context: Context): RecyclerView.Adapter<EventRecyclerViewAdapter.ViewHolder>() {
 
     companion object {
         const val loggingTag = "EventRecyclerViewAdapt"
     }
 
-//    private val mOnClickListener: View.OnClickListener
-
-    init {
-//        mOnClickListener = View.OnClickListener { v ->
-//            val item = v.tag as Event
-//            // Notify the active callbacks interface (the activity, if the fragment is attached to
-//            // one) that an item has been selected.
-//            // Maybe show the full info here?
-//            mListener?.onListFragmentInteraction(item)
-//        }
-    }
+    val mInflater = LayoutInflater.from(context)
+    var mEvents: List<Event> = Collections.emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.event_list_item, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(
+            LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.event_list_item, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.d(loggingTag, "onBindViewHolder called")
-        val item = mValues[position]
 
-        holder.eventIdWidget.setText(item.uid)
+        val item = mEvents.getOrNull(position) ?: return
+
+        holder.eventIdWidget.setText(item.uid.toString())
         holder.eventResource.text = item.resource
         holder.eventAction.text = item.action
-        if (item.resourceId != null) holder.eventResourceId.setText(item.resourceId as Int)
+
+        holder.eventResourceId.setText((item.resourceId ?: -1).toString())
+
         holder.eventDate.setText(SQLiteTypeConverter.fromOffsetDateTime(item.createdOn))
         holder.eventExtraInfo.setText(item.extraInfo)
-
-//        with(holder.mView) {
-//            tag = item
-//            setOnClickListener(mOnClickListener)
-//        }
     }
 
-    override fun getItemCount(): Int = mValues.size
+    fun setEvents(events: List<Event>) {
+        mEvents = events
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = mEvents.size
 
     inner class ViewHolder(mView: View): RecyclerView.ViewHolder(mView) {
-
-        val wrapper: RelativeLayout = mView.findViewById(R.id.eventListItemLayout)
         val eventIdWidget: EditText = mView.findViewById(R.id.eventId)
         val eventResource: TextView = mView.findViewById(R.id.eventResource)
         val eventAction: TextView = mView.findViewById(R.id.eventAction)
