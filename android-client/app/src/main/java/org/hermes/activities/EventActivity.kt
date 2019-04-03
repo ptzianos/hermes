793fca331da247b3c28dbe96177904a38491c7b3
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-04-03 11:08:44 +0200

    Implement EventActivity

--- android-client/app/src/main/java/org/hermes/activities/EventActivity.kt
+++ android-client/app/src/main/java/org/hermes/activities/EventActivity.kt
@@ -1,18 +1,52 @@
 package org.hermes.activities
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 
+import org.hermes.adapters.EventRecyclerViewAdapter
+import org.hermes.entities.Event
+import org.hermes.HermesRepository
 import org.hermes.R
+import org.hermes.viewmodels.EventViewModel
+import java.util.*
 
-class EventActivity : Activity() {
+
+class EventActivity : AppCompatActivity() {
 
     companion object {
         const val loggingTag = "EventLogActivity"
     }
 
+    private val repository by lazy { HermesRepository.getInstance(application) }
+    private var mEventViewModel: EventViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_list)
+
+        // TODO: Change the id to something more suitable
+        val recyclerView: RecyclerView = findViewById(R.id.fragment)
+        val adapter = EventRecyclerViewAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Get a new or existing ViewModel from the ViewModelProvider.
+        mEventViewModel = ViewModelProviders
+            .of(this)
+            .get(EventViewModel::class.java)
+
+        mEventViewModel?.getEvents()?.observe(this, Observer<List<Event>> {
+                Log.i(loggingTag, "onChanged called")
+                if (it == null) {
+                    adapter.setEvents(Collections.emptyList())
+                } else {
+                    adapter.setEvents(it)
+                }
+        })
     }
 }
