0d4d9cd4e607f173d47edde4a4cb4b56fb96b956
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-04-07 14:21:19 +0200

    Add implementation of Metrics 2.0 protocol

---
+++ android-client/app/src/main/java/org/hermes/Metric20.kt
@@ -0,0 +1,84 @@
+package org.hermes
+
+import org.threeten.bp.OffsetDateTime
+import java.lang.Exception
+
+/**
+ * An implementation of the Metrics2.0 spec for the Hermes client
+ * More info at: @see http://metrics20.org/spec/
+ *
+ * There are two tags that are mandatory:  unit and mtype.
+ */
+class Metric20(private val key: String,
+               private val value: Any,
+               private val timestamp: OffsetDateTime = OffsetDateTime.now()) {
+
+    class NoData: Exception()
+    class ForbiddenKey: Exception()
+    class KeysNotPresent : Exception()
+
+    enum class TagKey(val tagKey: String) {
+        HOST("host"),               // physical or virtual machine
+        HTTP_METHOD("http_method"), // the http method. like PUT, GET, etc.
+        HTTP_CODE("http_code"),     // 200, 404, etc
+        DEVICE("device"),           // block device, network device, …
+        UNIT("unit"),               // the unit something is expressed in (b/s, MB, etc).
+        WHAT("what"),               // the thing being measured, if the other tags don’t suffice. often same as metric key.
+        TYPE("type"),               // further describe the metric. type is a very generic word, only use it if you really don’t know anything better.
+        RESULT("result"),           // values: ok, fail, … (for http requests, http_code is probably more useful)
+        STAT("stat"),               // to clarify the statistical view
+        BIN_MAX("bin_max"),         // if your metrics are separated into bins by some numeric value, upper limit of a bin (like (statsd) histograms)
+        DIRECTION("direction"),     // in/out (not ‘tx’ or ‘rx’, more consistent)
+        MTYPE("mtype"),             // type of metric in terms of how the data should be interpreted.
+        FILE("file"),               // file (that generated a metric)
+        LINE("line"),               // line (that generated a metric)
+        ENV("env")                  // environment
+    }
+
+    private val meta: HashMap<String, String> = HashMap()
+    private val data: HashMap<String, String> = HashMap()
+
+    init {
+        meta["processed_by"] = "hermes"
+    }
+
+    fun setData(key: Metric20.TagKey, value: String): Metric20 {
+        data[key.tagKey] = value
+        return this
+    }
+
+    fun setMetadata(key: String, value: String): Metric20 {
+        if (key == "processed_by")
+            throw ForbiddenKey()
+        this.meta[key] = value
+        return this
+    }
+
+    fun toJsonString(): String {
+        fun mapToJson(mapping: Map<String, String>): String = mapping.map { "\"${it.key}\": \"${it.value}\"" }.joinToString()
+
+        if (data.isEmpty()) {
+            throw NoData()
+        } else if (!data.containsKey("unit") || !data.containsKey("mtype")) {
+            throw KeysNotPresent()
+        }
+        return "{" + mapToJson(data) + ", \"meta\": {" + mapToJson(meta) + "}}"
+    }
+
+    /**
+     * Carbon2.0 is a format that is compatible with with all Carbon-related daemons but it
+     * is also easily convertible to Metrics 2.0 format.
+     */
+    fun toCarbon20String(): String {
+        fun mapToJson(mapping: Map<String, String>, keyPrefix: String = ""): String = mapping
+            .map { "$keyPrefix${it.key}=${it.value}" }
+            .joinToString(separator = ";")
+
+        if (data.isEmpty()) {
+            throw NoData()
+        } else if (!data.containsKey("unit") || !data.containsKey("mtype")) {
+            throw KeysNotPresent()
+        }
+        return "$key;${mapToJson(data)};${mapToJson(meta, "meta.")} ${timestamp.toEpochSecond()} $value"
+    }
+}
--- android-client/app/src/test/java/org/hermes/Metric20Test.kt
+++ android-client/app/src/test/java/org/hermes/Metric20Test.kt
@@ -1,5 +1,33 @@
 package org.hermes
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.threeten.bp.OffsetDateTime
 
-internal class Metric20Test
+class Metric20Test {
+    @Test
+    fun `check valid metric to metrics 20`() {
+        assertEquals(
+            "{\"mtype\": \"b/s\", \"unit\": \"20\", \"meta\": {\"processed_by\": \"hermes\"}}",
+            Metric20("host.some.device", "1.0")
+                .setData(Metric20.TagKey.MTYPE, "b/s")
+                .setData(Metric20.TagKey.UNIT, "20")
+                .toJsonString()
+        )
+    }
+
+    @Test
+    fun `check valid metric to carbon 20`() {
+        val timestamp = OffsetDateTime.now()
+        val expectedString = "host.some.device;mtype=b/s;unit=20;meta.processed_by=hermes ${timestamp.toEpochSecond()} 1.0"
+        assertEquals(
+            expectedString,
+            Metric20("host.some.device", "1.0", timestamp)
+                .setData(Metric20.TagKey.MTYPE, "b/s")
+                .setData(Metric20.TagKey.UNIT, "20")
+                .toCarbon20String()
+        )
+    }
+
+    // TODO: Add more tests
+}
