de08635fa52cca82222fa59b2d6a6b1d77bea74b
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-04-25 14:05:05 +0200

    Cleanup event list item UI

--- android-client/app/src/main/java/org/hermes/viewholders/EventLogViewHolder.kt
+++ android-client/app/src/main/java/org/hermes/viewholders/EventLogViewHolder.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.view.View
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import org.threeten.bp.format.DateTimeFormatter
@@ -14,30 +13,33 @@ import org.hermes.activities.EventActivity
 import org.hermes.entities.Event
 
 
-class EventLogViewHolder(parent : View) : ViewHolder(parent) {
+class EventLogViewHolder(val parent : View) : ViewHolder(parent), View.OnClickListener {
 
     private val eventIdWidget: TextView = parent.findViewById(R.id.eventId)
     private val eventResource: TextView = parent.findViewById(R.id.eventResource)
     private val eventAction: TextView = parent.findViewById(R.id.eventAction)
-    private val eventResourceId: EditText = parent.findViewById(R.id.eventResourceId)
-    private val eventDate: EditText = parent.findViewById(R.id.eventDate)
-    private val eventExtraInfo: EditText = parent.findViewById(R.id.eventExtraInfo)
-    private val eventArrow: ImageButton = parent.findViewById(R.id.eventArrow)
+    private val eventResourceId: TextView = parent.findViewById(R.id.eventResourceId)
+    private val eventDate: TextView = parent.findViewById(R.id.eventDate)
+    private lateinit var context: Context
+    private lateinit var item: Event
 
     fun bind(item: Event, context: Context) {
         eventIdWidget.text = item.uid.toString()
         eventResource.text = item.resource
         eventAction.text = item.action
 
-        eventResourceId.setText((item.resourceId ?: -1).toString())
+        eventResourceId.text = (item.resourceId ?: -1).toString()
 
-        eventDate.setText(item.createdOn.format(DateTimeFormatter.RFC_1123_DATE_TIME))
-        eventExtraInfo.setText(item.extraInfo)
+        eventDate.text = item.createdOn.format(DateTimeFormatter.RFC_1123_DATE_TIME)
 
-        eventArrow.setOnClickListener {
-            val intent = Intent(context, EventActivity::class.java)
-            intent.putExtra("eventId", item.uid)
-            context.startActivity(intent)
-        }
+        this.context = context
+        this.item = item
+        parent.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        context.startActivity(Intent(context, EventActivity::class.java).apply {
+            putExtra("eventId", item.uid)
+        })
     }
 }
