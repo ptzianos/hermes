c5aeab3ab509185a14d0d7fe76069e7555f074ff
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-24 16:17:15 +0200

    Do not close session once a request has finished

--- marketplace/src/hermes/signals.py
+++ marketplace/src/hermes/signals.py
@@ -6,8 +6,11 @@ def register_signals(app: Flask):
 
     @app.after_request
     def after_request(response: Response):
-        """Automatically commit the User session object changes once a request is finished"""
+        """Automatically commit the User session object changes once a request is finished.
+
+        It does not close the session object, because otherwise an exception will be thrown
+        every time the fields of the PersistentSession object are accessed since it will no
+        longer be part of an active session.
+        """
         g.db_session.commit()
-        # Once the commit is finished then cleanup the session object itself
-        g.db_session.remove()
         return response
