2492e39c70754c3e35420f9877e42382761c7fb4
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-24 16:20:00 +0200

    Fix duplicate fixtures for flask app

--- marketplace/test/fixtures/flask.py
+++ marketplace/test/fixtures/flask.py
@@ -7,15 +7,22 @@ from flask import Flask
 from flask.logging import create_logger
 from flask.testing import FlaskClient
 
+from hermes import config
 from hermes.db import init_db
 from hermes.signals import register_signals
 
 
 @pytest.fixture(scope='session')
 def flask_app():
+    """Bootstraps a flask application and initializes all the necessary infrastructure for it to work.
+
+    It registers all the signals and views of the application and sets up the Session mechanism.
+    """
     hermes_test_app = Flask(__name__)
     hermes_test_app.config['TESTING'] = True
-    db_fd, hermes_test_app.config['DATABASE'] = tempfile.mkstemp()
+    db_fd, db_path = tempfile.mkstemp()
+    hermes_test_app.config['DATABASE_FILE_PATH'] = db_path
+    hermes_test_app.config['DATABASE'] = config.SQL_ALCHEMY_SQLITE_ABS_PATH_TEMPLATE.format(abs_file_path=db_path)
 
     # The database needs to be initialized before the session object
     # because the session object will need the models of the app
@@ -23,19 +30,23 @@ def flask_app():
     with hermes_test_app.app_context():
         init_db()
 
-    from hermes.user.session import HermesSession
-    hermes_test_app.session_interface = HermesSession()
+        from hermes.user.session import HermesSession
+        hermes_test_app.session_interface = HermesSession()
+
+        register_signals(hermes_test_app)
+
+        hermes_test_app.Base.metadata.create_all()
+
+        from hermes.views import register_views_to_app
+        register_views_to_app(hermes_test_app)
 
     log = create_logger(hermes_test_app)
     log.setLevel(logging.DEBUG)
-    register_signals(hermes_test_app)
 
-    hermes_test_app.Base.metadata.create_all()
-
-    yield hermes_test_app.test_client()
+    yield hermes_test_app
 
     os.close(db_fd)
-    os.unlink(hermes_test_app.config['DATABASE'])
+    os.unlink(hermes_test_app.config['DATABASE_FILE_PATH'])
 
 
 @pytest.fixture(scope='session')
