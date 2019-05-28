98c0db143cb19c9e55a604710975010f81ea7046
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-28 18:49:04 +0200

    Return the PublicKeyVerificationRequest object from user registration util

--- marketplace/tests/hermes/user/utils.py
+++ marketplace/tests/hermes/user/utils.py
@@ -5,14 +5,16 @@ from Crypto.PublicKey import ECC, RSA
 from flask.testing import FlaskClient
 
 if TYPE_CHECKING:
-    from hermes.user.models import EmailAddress, PublicKey, User
+    from hermes.user.models import (EmailAddress, PublicKey,
+                                    PublicKeyVerificationRequest,
+                                    User)
 
 
 def register_user(
     api_client: FlaskClient,
     key: Union[ECC.EccKey, RSA.RsaKey],
     passwd: str = '', name: str = '', email: str = ''
-) -> Tuple['User', 'PublicKey', Optional['EmailAddress'], str, str]:
+) -> Tuple['User', 'PublicKey', Optional['EmailAddress'], 'PublicKeyVerificationRequest']:
     """Registers a new user.
 
     Returns the user and the verification token message and id and the email
@@ -35,15 +37,22 @@ def register_user(
 
     from flask import g
     from hermes.user.controllers import resolve_user
-    from hermes.user.models import EmailAddress, PublicKey
+    from hermes.user.models import (EmailAddress, PublicKey,
+                                    PublicKeyVerificationRequest)
 
     new_user = resolve_user(resp.json['uuid'])
     assert new_user is not None
     pk = g.db_session.query(PublicKey).filter_by(owner_id=new_user.id).first()
-    email = (g.db_session
-             .query(EmailAddress)
-             .filter_by(owner_id=new_user.id)
-             .first()
-             if email else None)
-    return (new_user, pk, email, resp.json['public_key_verification_token'],
-            resp.json['public_key_verification_message'])
+    email: Optional[EmailAddress] = (g.db_session
+                                     .query(EmailAddress)
+                                     .filter_by(owner_id=new_user.id)
+                                     .first()
+                                     if email else None)
+
+    verification_request: PublicKeyVerificationRequest = (
+        g.db_session
+         .query(PublicKeyVerificationRequest)
+         .filter_by(token=resp.json['public_key_verification_token'])
+         .first()
+    )
+    return new_user, pk, email, verification_request
