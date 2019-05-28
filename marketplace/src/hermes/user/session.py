c9d1598767ac30bc9e304a6cd317856c97470741
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-28 17:54:02 +0200

    Change session implementation to handle APITokens also

--- marketplace/src/hermes/user/session.py
+++ marketplace/src/hermes/user/session.py
@@ -1,4 +1,5 @@
 import base64
+from typing import Optional
 
 import requests
 from flask import current_app, Flask, g, make_response, Request, Response
@@ -8,29 +9,35 @@ from hermes.user.models import APIToken, ProxySession, SessionToken
 
 
 class HermesSession(SessionInterface):
-    """Implements the Session Flask interface with SQLAlchemy Session model as a backend
-    """
+    """Implements the Session Flask interface using the Session model."""
+
     db_session_initialized = False
 
-    def open_session(self, app: Flask, request: Request, *args) -> 'ProxySession':
-        session_token_str = request.cookies.get(app.session_cookie_name) or None
+    def open_session(
+        self, app: Flask, request: Request, *args
+    ) -> 'ProxySession':
         # Create a db_session only if one has not been created yet
         if not getattr(g, 'db_session'):
             db_session = g.db_session = current_app.new_db_session_instance()
             self.db_session_initialized = True
         else:
             db_session = g.db_session
+
+        session_token_str: Optional[str] = \
+            request.cookies.get(app.session_cookie_name, None)
+        authorization_str: Optional[str] = \
+            request.headers.get('Authorization', None)
+
         if session_token_str:
-            user_session = (db_session
-                            .query(SessionToken)
-                            .filter_by(token=session_token_str, expired=False)
-                            .first())  # type: SessionToken
-            if not user_session or user_session.is_expired:
-                session_token_str = None
-            else:
+            user_session: SessionToken = (db_session
+                                          .query(SessionToken)
+                                          .filter_by(token=session_token_str,
+                                                     expired=False)
+                                          .first())
+            if user_session and user_session.is_expired:
                 user_session.refresh()
-        authorization_str = request.headers.get('Authorization')  # type: str
-        if authorization_str:
+                return user_session.proxy
+        elif authorization_str:
             base64_token = ''
             if authorization_str.startswith('Basic'):
                 base64_token = authorization_str.split('Basic ')[-1]
@@ -38,21 +45,26 @@ class HermesSession(SessionInterface):
                 base64_token = authorization_str.split('Bearer ')[-1]
             if not base64_token:
                 raise make_response('', requests.codes.forbidden)
-            token = base64.decodebytes(base64_token.encode('ascii', errors='ignore'))
-            api_token = db_session.query(APIToken).filter_by(token=token, expired=False)
-            if not api_token:
-                return make_response()
-            # TODO: Fix the session object. Probably create one but don't save it
-            return api_token.owner
+            token = (base64
+                     .decodebytes(base64_token.encode('utf-8',
+                                                      errors='ignore'))
+                     .decode())
+            api_token: APIToken = (db_session
+                                   .query(APIToken)
+                                   .filter_by(token=token, expired=False)
+                                   .first())
+            if not api_token or api_token.is_expired:
+                return make_response(requests.codes.forbidden)
+            return api_token.proxy
 
-        if session_token_str is None:
-            user_session = SessionToken()
-            user_session.refresh()
-            db_session.add(user_session)
-
+        user_session = SessionToken()
+        user_session.refresh()
+        db_session.add(user_session)
         return user_session.proxy
 
-    def save_session(self, app: Flask, session: 'ProxySession', response: Response) -> None:
+    def save_session(
+        self, app: Flask, session: 'ProxySession', response: Response
+    ) -> None:
         if not response:
             return
         # TODO: Work on fixing permanent sessions
