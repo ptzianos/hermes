b4387cb9d6b95060f65cba3d09700556c4c9fd34
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-28 15:15:03 +0200

    Do not setup new db session when registering test user

--- marketplace/tests/hermes/user/utils.py
+++ marketplace/tests/hermes/user/utils.py
@@ -2,7 +2,6 @@ from typing import Optional, TYPE_CHECKING, Tuple, Union
 
 import requests
 from Crypto.PublicKey import ECC, RSA
-from flask import Flask
 from flask.testing import FlaskClient
 
 if TYPE_CHECKING:
@@ -10,7 +9,7 @@ if TYPE_CHECKING:
 
 
 def register_user(
-    flask_app: Flask, api_client: FlaskClient,
+    api_client: FlaskClient,
     key: Union[ECC.EccKey, RSA.RsaKey],
     passwd: str = '', name: str = '', email: str = ''
 ) -> Tuple['User', 'PublicKey', Optional['EmailAddress'], str, str]:
@@ -33,16 +32,18 @@ def register_user(
         'public_key_type': 'ecdsa',
     })
     assert resp.status_code == requests.codes.ok
-    with flask_app.app_context_and_db_session():
-        from flask import g
-        from hermes.user.controllers import resolve_user
-        from hermes.user.models import EmailAddress, PublicKey
-        new_user = resolve_user(resp.json['uuid'])
-        assert new_user is not None
-        pk = g.db_session.query(PublicKey).filter_by(owner_id=new_user.id).first()
-        return (new_user,
-                pk,
-                (g.db_session.query(EmailAddress).filter_by(owner_id=new_user.id).first()
-                 if email else None),
-                resp.json['public_key_verification_token'],
-                resp.json['public_key_verification_message'])
+
+    from flask import g
+    from hermes.user.controllers import resolve_user
+    from hermes.user.models import EmailAddress, PublicKey
+
+    new_user = resolve_user(resp.json['uuid'])
+    assert new_user is not None
+    pk = g.db_session.query(PublicKey).filter_by(owner_id=new_user.id).first()
+    email = (g.db_session
+             .query(EmailAddress)
+             .filter_by(owner_id=new_user.id)
+             .first()
+             if email else None)
+    return (new_user, pk, email, resp.json['public_key_verification_token'],
+            resp.json['public_key_verification_message'])
