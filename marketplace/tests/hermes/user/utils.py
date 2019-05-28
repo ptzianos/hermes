1bdbfabf0ff142938f1fb6cb11178fa62acefd65
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-28 13:20:24 +0200

    Return public key verification data and improve type checking for test user registration util

--- marketplace/tests/hermes/user/utils.py
+++ marketplace/tests/hermes/user/utils.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Tuple, Union
+from typing import Optional, TYPE_CHECKING, Tuple, Union
 
 import requests
 from Crypto.PublicKey import ECC, RSA
@@ -10,10 +10,15 @@ if TYPE_CHECKING:
 
 
 def register_user(
-        flask_app: Flask, api_client: FlaskClient,
-        key: Union[ECC.EccKey, RSA.RsaKey],
-        passwd: str = '', name: str = '', email: str = ''
-) -> Tuple['User', 'PublicKey', 'EmailAddress']:
+    flask_app: Flask, api_client: FlaskClient,
+    key: Union[ECC.EccKey, RSA.RsaKey],
+    passwd: str = '', name: str = '', email: str = ''
+) -> Tuple['User', 'PublicKey', Optional['EmailAddress'], str, str]:
+    """Registers a new user.
+
+    Returns the user and the verification token message and id and the email
+    object if an email was provided.
+    """
     if isinstance(key, ECC.EccKey):
         public_key = key.export_key(format='PEM')
     elif isinstance(key, RSA.RsaKey):
@@ -35,6 +40,9 @@ def register_user(
         new_user = resolve_user(resp.json['uuid'])
         assert new_user is not None
         pk = g.db_session.query(PublicKey).filter_by(owner_id=new_user.id).first()
-        return (new_user, pk,
+        return (new_user,
+                pk,
                 (g.db_session.query(EmailAddress).filter_by(owner_id=new_user.id).first()
-                 if email else None))
+                 if email else None),
+                resp.json['public_key_verification_token'],
+                resp.json['public_key_verification_message'])
