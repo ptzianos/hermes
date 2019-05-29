00a217a5b4f85fbf804b926245f4726b3a7ae005
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-29 12:29:49 +0200

    Use test view request helper for user view tests

--- marketplace/tests/hermes/user/test_views.py
+++ marketplace/tests/hermes/user/test_views.py
@@ -1,4 +1,3 @@
-import base64
 from typing import Iterator
 
 import pytest
@@ -7,20 +6,18 @@ from Crypto.Hash import SHA3_512
 from Crypto.PublicKey.ECC import EccKey, import_key as import_ecdsa_key
 from Crypto.PublicKey.RSA import RsaKey
 from Crypto.Signature.DSS import new as new_dss_sig_scheme
-from flask.testing import FlaskClient
 
-from tests.hermes.user.utils import register_user
+from tests.hermes.user.utils import register_user, register_user_and_get_token
+from tests.utils import get, post
 
 
 @pytest.mark.usefixtures('sqlalchemy_test_session')
-def test_register_view(api_client: FlaskClient,
-                       random_email: Iterator[str],
+def test_register_view(random_email: Iterator[str],
                        ecdsa_key_pair: Iterator[EccKey],
                        rsa_key_pair: Iterator[RsaKey]) -> None:
     from hermes.user.controllers import resolve_user
 
-    # Register a standard user with an ECDSA key
-    resp = api_client.post('/api/v1/users/register', data={
+    resp = post('/api/v1/users/register', data={
         'email': next(random_email),
         'public_key': next(ecdsa_key_pair).export_key(format='PEM'),
         'public_key_type': 'ecdsa',
@@ -35,7 +32,7 @@ def test_register_view(api_client: FlaskClient,
     assert user.uuid == user2.uuid
 
     # Try to register a user with a duplicate email
-    resp = api_client.post('/api/v1/users/register', data={
+    resp = post('/api/v1/users/register', data={
         'email': resp.json['email'],
         'public_key': next(ecdsa_key_pair).export_key(format='PEM'),
         'public_key_type': 'ecdsa',
@@ -43,7 +40,7 @@ def test_register_view(api_client: FlaskClient,
     assert resp.status_code == requests.codes.bad_request
 
     # Register a user with just an RSA key
-    resp = api_client.post('/api/v1/users/register', data={
+    resp = post('/api/v1/users/register', data={
         'public_key': next(rsa_key_pair).export_key().decode(),
         'public_key_type': 'rsa',
     })
@@ -51,7 +48,7 @@ def test_register_view(api_client: FlaskClient,
     assert resolve_user(resp.json['uuid']) is not None
 
     # Try to register a user with the wrong type of key
-    resp = api_client.post('/api/v1/users/register', data={
+    resp = post('/api/v1/users/register', data={
         'public_key': next(rsa_key_pair).export_key().decode(),
         'public_key_type': 'ecdsa',
     })
@@ -60,12 +57,9 @@ def test_register_view(api_client: FlaskClient,
 
 @pytest.mark.usefixtures('sqlalchemy_test_session')
 def test_generate_api_token_view(
-    flask_app,
-    api_client: FlaskClient,
     ecdsa_key_pair: Iterator[EccKey]
 ) -> None:
-    user, pk, _, verification_request = register_user(api_client,
-                                                      next(ecdsa_key_pair))
+    user, pk, _, verification_request = register_user(next(ecdsa_key_pair))
 
     msg_hash = (SHA3_512.new()
                 .update(verification_request.original_message.encode()))
@@ -74,37 +68,24 @@ def test_generate_api_token_view(
     signature = sig_scheme.sign(msg_hash)
     token_endpoint = ('/api/v1/users/{user_uuid}/tokens/'
                       .format(user_uuid=user.uuid))
-    api_client.delete_cookie('localhost.local', flask_app.session_cookie_name)
-    resp = api_client.post(token_endpoint, data={
+    resp = post(token_endpoint, data={
         'proof_of_ownership_request': verification_request.token,
         'proof_of_ownership': signature.hex(),
-    })
-
+    }, no_cookies=True)
     assert resp.status_code == requests.codes.ok
     assert resp.json.get('token') is not None
 
     api_token = resp.json.get('token')
-    api_client.delete_cookie('localhost.local', flask_app.session_cookie_name)
-    resp = api_client.get(
-        '/api/v1/users/me',
-        headers={
-            'Authorization': 'Bearer ' + base64.encodebytes(api_token.encode())
-                                               .decode('utf-8')
-                                               .strip()
-        })
-
+    resp = get('/api/v1/users/me', api_token=api_token, no_cookies=True)
     assert resp.status_code == requests.codes.ok
     assert resp.json['uuid'] == user.uuid
 
 
 @pytest.mark.usefixtures('sqlalchemy_test_session')
 def test_cant_generate_token_twice_with_same_verification_request(
-    flask_app,
-    api_client: FlaskClient,
     ecdsa_key_pair: Iterator[EccKey]
 ) -> None:
-    user, pk, _, verification_request = register_user(api_client,
-                                                      next(ecdsa_key_pair))
+    user, pk, _, verification_request = register_user(next(ecdsa_key_pair))
 
     msg_hash = (SHA3_512.new()
                 .update(verification_request.original_message.encode()))
@@ -117,30 +98,22 @@ def test_cant_generate_token_twice_with_same_verification_request(
         'proof_of_ownership_request': verification_request.token,
         'proof_of_ownership': signature.hex(),
     }
-    api_client.delete_cookie('localhost.local', flask_app.session_cookie_name)
-    resp = api_client.post(token_endpoint, data=post_data)
-
+    resp = post(token_endpoint, data=post_data, no_cookies=True)
     assert resp.status_code == requests.codes.ok
     assert resp.json.get('token') is not None
 
-    api_client.delete_cookie('localhost.local', flask_app.session_cookie_name)
-    resp = api_client.post(token_endpoint, data=post_data)
-
+    resp = post(token_endpoint, data=post_data, no_cookies=True)
     assert resp.status_code == requests.codes.forbidden
 
 
 @pytest.mark.usefixtures('sqlalchemy_test_session')
 def test_new_verification_request_expires_old_one(
-    flask_app,
-    api_client: FlaskClient,
     ecdsa_key_pair: Iterator[EccKey]
 ) -> None:
-    user, pk, _, verification_request = register_user(api_client,
-                                                      next(ecdsa_key_pair))
+    user, pk, _, verification_request = register_user(next(ecdsa_key_pair))
     msg_endpoint = ('/api/v1/users/{user_uuid}/keys/{key_id}/message'
                     .format(user_uuid=user.uuid, key_id=pk.uuid))
-    api_client.delete_cookie('localhost.local', flask_app.session_cookie_name)
-    verification_request_resp = api_client.get(msg_endpoint)
+    verification_request_resp = get(msg_endpoint, no_cookies=True)
 
     assert verification_request_resp.status_code == requests.codes.ok
     assert verification_request_resp.json.get('public_key_verification_token') is not None
@@ -155,11 +128,10 @@ def test_new_verification_request_expires_old_one(
     signature = sig_scheme.sign(msg_hash)
     token_endpoint = ('/api/v1/users/{user_uuid}/tokens/'
                       .format(user_uuid=user.uuid))
-    api_client.delete_cookie('localhost.local', flask_app.session_cookie_name)
-    resp = api_client.post(token_endpoint, data={
+    resp = post(token_endpoint, data={
         'proof_of_ownership_request': verification_request.token,
         'proof_of_ownership': signature.hex(),
-    })
+    }, no_cookies=True)
     assert resp.status_code == requests.codes.forbidden
 
     # New verification request must be usable now
