39d52b7c300f92137df37b016dedf65f2d60ebe1
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-05-29 09:46:24 +0200

    Allow test to create admin user

--- marketplace/tests/hermes/user/utils.py
+++ marketplace/tests/hermes/user/utils.py
@@ -1,28 +1,31 @@
 from typing import Optional, TYPE_CHECKING, Tuple, Union
 
 import requests
-from Crypto.PublicKey import ECC, RSA
+from Crypto.Hash import SHA3_512
+from Crypto.PublicKey.ECC import EccKey, import_key as import_ecdsa_key
+from Crypto.PublicKey.RSA import RsaKey
+from Crypto.Signature.DSS import new as new_dss_sig_scheme
 from flask.testing import FlaskClient
 
 if TYPE_CHECKING:
-    from hermes.user.models import (EmailAddress, PublicKey,
+    from hermes.user.models import (APIToken, EmailAddress, PublicKey,
                                     PublicKeyVerificationRequest,
                                     User)
 
 
 def register_user(
     api_client: FlaskClient,
-    key: Union[ECC.EccKey, RSA.RsaKey],
-    passwd: str = '', name: str = '', email: str = ''
+    key: Union[EccKey, RsaKey],
+    passwd: str = '', name: str = '', email: str = '', admin: bool = False
 ) -> Tuple['User', 'PublicKey', Optional['EmailAddress'], 'PublicKeyVerificationRequest']:
     """Registers a new user.
 
-    Returns the user and the verification token message and id and the email
+    Returns the user, the verification token request model and the email
     object if an email was provided.
     """
-    if isinstance(key, ECC.EccKey):
+    if isinstance(key, EccKey):
         public_key = key.export_key(format='PEM')
-    elif isinstance(key, RSA.RsaKey):
+    elif isinstance(key, RsaKey):
         public_key = key.export_key().decode()
     else:
         raise Exception('Key must be either RSA or ECDSA')
@@ -42,6 +45,9 @@ def register_user(
 
     new_user = resolve_user(resp.json['uuid'])
     assert new_user is not None
+    if admin:
+        new_user.admin = True
+
     pk = g.db_session.query(PublicKey).filter_by(owner_id=new_user.id).first()
     email: Optional[EmailAddress] = (g.db_session
                                      .query(EmailAddress)
