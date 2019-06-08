8b87011686e4cb8debe7a54ed29587bcdbf0e30b
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-06-09 01:09:52 +0200

    Refactor server to initialize database with app context

--- marketplace/src/hermes/server.py
+++ marketplace/src/hermes/server.py
@@ -11,10 +11,17 @@ from hermes.views import register_views_to_app
 
 app = Flask(__name__)
 app.config.from_object(hermes.config)
-init_db()
 app.session_interface = HermesSession()
 log = create_logger(app)
 log.setLevel(logging.INFO)
 register_signals(app)
 register_views_to_app(app)
 
+with app.app_context():
+    init_db()
+    from hermes.ad.models import Ad
+    from hermes.user.models import (APIToken, AuthenticationToken, BaseToken,
+                                    EmailAddress, EmailVerificationToken,
+                                    PasswordResetToken, PublicKey,
+                                    PublicKeyVerificationRequest, SessionToken,
+                                    User)
