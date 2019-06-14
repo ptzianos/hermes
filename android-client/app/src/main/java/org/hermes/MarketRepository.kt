4a4aded1d99fbc7c43ebca5415eb80478122b40d
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-06-14 13:51:00 +0200

    Add repository for issuing calls to the marketplace

---
+++ android-client/app/src/main/java/org/hermes/MarketRepository.kt
@@ -0,0 +1,65 @@
+package org.hermes
+
+import android.util.Log
+import javax.inject.Inject
+import javax.inject.Singleton
+import retrofit2.Retrofit
+
+import org.hermes.entities.User
+import org.hermes.market.APIToken
+import org.hermes.market.HermesMarketV1
+
+
+@Singleton
+class MarketRepository @Inject constructor(
+    private val cryptoRepository: CryptoRepository,
+    private val db: HermesRoomDatabase,
+    private val retroBuilder: Retrofit.Builder
+) {
+
+    private val loggingTag = "MarketRepository"
+
+    fun registerUser(domain: String = "hermes-data.io"): Boolean {
+        Log.i(loggingTag, "Deleting any user in the database for domain $domain")
+        db.userDao().deleteByDomain(domain)
+        Log.i(loggingTag,"Registering user to the marketplace")
+        val apiService = retroBuilder.baseUrl("https://$domain")
+            .build()
+            .create(HermesMarketV1::class.java)
+        val publicKeyDer = cryptoRepository.publicKeyDER()
+        val registrationResp = apiService
+            .register(publicKeyDer, "ECDSA")
+            .execute()
+        if (!registrationResp.isSuccessful) {
+            Log.e(loggingTag, "Could not register user with marketplace: " +
+                    "${registrationResp.errorBody()}")
+            return false
+        } else {
+            val registrationResponse = registrationResp.body()!!
+            Log.i(loggingTag, "Verifying user's key with market https://$domain")
+            val apiToken = createToken(
+                apiService,
+                registrationResponse.uuid,
+                registrationResponse.publicKeyVerificationToken,
+                registrationResponse.publicKeyVerificationMessage)
+            if (apiToken != null) {
+                Log.i(loggingTag, "Storing user with ${registrationResponse.uuid} from market " +
+                        "https://$domain to the database")
+                db.userDao().insertAll(User(
+                    marketUUID = registrationResponse.uuid, name = registrationResponse.name,
+                    domain = domain, token = apiToken.token
+                ))
+            }
+            return apiToken != null
+        }
+    }
+
+    fun createToken(apiService: HermesMarketV1, userId: String, proofOfOwnershipToken: String,
+                    proofOfOwnershipMessage: String): APIToken? {
+        val signedMessage = cryptoRepository.signMessage(proofOfOwnershipMessage)
+        val verificationResp = apiService
+            .createToken(userId, proofOfOwnershipToken, signedMessage)
+            .execute()
+        return verificationResp.body()
+    }
+}
