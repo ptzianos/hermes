706bf314a5061effa7d496bd3458ff3216ebb93d
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-06-14 15:18:41 +0200

    Add request loggers and exception logging in api server

--- marketplace/src/hermes/signals.py
+++ marketplace/src/hermes/signals.py
@@ -1,4 +1,7 @@
-from flask import Flask, g
+import traceback
+from time import strftime
+
+from flask import current_app, Flask, g, request
 from flask.wrappers import Response
 
 
@@ -6,11 +9,26 @@ def register_signals(app: Flask):
 
     @app.after_request
     def after_request(response: Response):
-        """Automatically commit the User session object changes once a request is finished.
+        """Commit the User session object changes once a request is finished.
 
-        It does not close the session object, because otherwise an exception will be thrown
-        every time the fields of the PersistentSession object are accessed since it will no
-        longer be part of an active session.
+        It does not close the session object, because otherwise an exception
+        will be thrown every time the fields of the PersistentSession object
+        are accessed since it will no longer be part of an active session.
         """
+        timestamp = strftime('[%Y-%b-%d %H:%M]')
+        current_app.log.info('%s %s %s %s %s %s', timestamp,
+                             request.remote_addr, request.method,
+                             request.scheme, request.full_path,
+                             response.status)
+
         g.db_session.commit()
         return response
+
+    @app.errorhandler(Exception)
+    def exceptions(e):
+        tb = traceback.format_exc()
+        timestamp = strftime('[%Y-%b-%d %H:%M]')
+        current_app.log.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
+                              timestamp, request.remote_addr, request.method,
+                              request.scheme, request.full_path, tb)
+        return e.status_code
