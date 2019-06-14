61cfa06efc9a65cc773ee2ddd187854be59ea3c7
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-06-14 14:06:00 +0200

    Add factory for Retrofit clients

--- android-client/app/src/main/java/org/hermes/di/HermesModule.kt
+++ android-client/app/src/main/java/org/hermes/di/HermesModule.kt
@@ -4,11 +4,16 @@ import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.room.Room
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import java.security.KeyStore
 import javax.inject.Named
 import javax.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 import org.hermes.HermesRoomDatabase
 import org.hermes.R
@@ -38,5 +43,19 @@ class HermesModule {
         return KeyStore.getInstance("AndroidKeyStore").apply {
             load(null)
         }
+    }
+
+    @Provides fun marketServiceApi(): Retrofit.Builder {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(
+                OkHttpClient.Builder()
+                    .connectTimeout(20, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .build())
     }
 }
