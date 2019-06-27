9949e4b167d088515143277bac0fc15c1229d994
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-06-27 15:19:39 +0200

    Prevent errors related to updating the UI from a background thread

--- android-client/app/src/main/java/org/hermes/activities/SetupLoadActivity.kt
+++ android-client/app/src/main/java/org/hermes/activities/SetupLoadActivity.kt
@@ -2,7 +2,12 @@ package org.hermes.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import dagger.Module
 import dagger.android.AndroidInjection
 import javax.inject.Inject
@@ -32,6 +37,12 @@ class SetupLoadActivity : BaseActivity() {
 
     private val setupLoaderMessage: TextView by lazy { findViewById<TextView>(R.id.setupLoaderMessage) }
 
+    private val handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(inputMessage: Message) {
+            setupLoaderMessage.text = inputMessage.obj as String
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
@@ -41,15 +52,26 @@ class SetupLoadActivity : BaseActivity() {
 
     private fun startPipeline(pin: String) {
         CoroutineScope(BACKGROUND.asCoroutineDispatcher()).launch {
-            generateCredentials(pin)
-            setupLoaderMessage.text = "Registering agent with the marketplace. Please don't close the app"
+            handler
+                .obtainMessage(0, "Generating seed and key pair. Please don't close app")
+                .apply { sendToTarget() }
+            cryptoRepository.generateCredentials(pin)
+            cryptoRepository.unseal(pin)
+            handler
+                .obtainMessage(0, "Registering agent with the marketplace. Please don't close the app")
+                .apply { sendToTarget() }
             if (!marketRepository.registerUser()) {
-                showErrorMessage()
+                handler
+                    .obtainMessage(0, "There was an error. Please retry later to setup and make sure you " +
+                            "have an active Internet connection")
+                    .apply { sendToTarget() }
                 rollbackCredentials()
                 delay(5 * 1000)
                 goToSetupPage()
             } else {
-                setupLoaderMessage.text = "Done with the registration. Have fun!"
+                handler
+                    .obtainMessage(0, "Done with the registration. Have fun!")
+                    .apply { sendToTarget() }
                 delay(1000)
                 metadataRepository.startLedgerService()
                 goToDashboard()
@@ -57,18 +79,7 @@ class SetupLoadActivity : BaseActivity() {
         }
     }
 
-    private fun generateCredentials(pin: String) {
-        setupLoaderMessage.text = "Generating seed and key pair. Please don't close app"
-        cryptoRepository.generateCredentials(pin)
-        cryptoRepository.unseal(pin)
-    }
-
     private fun rollbackCredentials() = cryptoRepository.clearCredentials()
-
-    private fun showErrorMessage() {
-        setupLoaderMessage.text = "There was an error. Please retry later to setup " +
-                "and make sure you have an active Internet connection"
-    }
 
     private fun goToSetupPage() {
         startActivity(Intent(this, SetupActivity::class.java))
