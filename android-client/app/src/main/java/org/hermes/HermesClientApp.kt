029696d61f1564ccf45bba03c2b014cb410c6bf5
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-07-03 13:58:40 +0200

    Add system wide handler for toasts

--- android-client/app/src/main/java/org/hermes/HermesClientApp.kt
+++ android-client/app/src/main/java/org/hermes/HermesClientApp.kt
@@ -1,5 +1,9 @@
 package org.hermes
 
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -18,6 +22,15 @@ class HermesClientApp : DaggerApplication(), HasSupportFragmentInjector {
     lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
 
     lateinit var daggerHermesComponent: HermesComponent
+
+
+    val toastHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(inputMessage: Message) {
+            try {
+                Toast.makeText(applicationContext, inputMessage.obj as String, Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {}
+        }
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         daggerHermesComponent = DaggerHermesComponent
