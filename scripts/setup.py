f9a81260647bf9d0aa73ee3b8994894656d28575
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-08-27 18:33:26 +0300

    Add setup script for carbon ledger

---
+++ scripts/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    readme = f.read()
+
+setup(
+    name='carbon-ledger',
+    version='0.1.0',
+    description='Daemon for following streams of data from ledgers and redirecting them to Carbon',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Pavlos Tzianos',
+    author_email='pavlos.tzianos@gmail.com',
+    url='',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src', exclude=('tests', 'docs')),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
