1127708dcab5831d14de4b437fe480688dee2bdc
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-09-02 15:07:02 +0300

    Change Python compatibility version to 3.6

--- carbon-ledger/setup.py
+++ carbon-ledger/setup.py
@@ -22,7 +22,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
