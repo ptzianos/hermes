75768eab215b00c112432cda4442fd6aa2e12891
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-09-13 20:17:16 +0200

    Rename SHAHash to Hasher

--- android-client/app/src/main/java/org/hermes/crypto/SHAHash.kt
+++ android-client/app/src/main/java/org/hermes/crypto/SHAHash.kt
@@ -2,7 +2,7 @@ package org.hermes.crypto
 
 import java.security.MessageDigest
 
-open class SHAHash(val impl: MessageDigest) {
+open class Hasher(val impl: MessageDigest) {
 
     fun hash(b: ByteArray): ByteArray {
         return impl.digest(b)
