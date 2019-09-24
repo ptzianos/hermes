baee4c6bd2d6ca0b483117fd99d41814426a6ea1
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-09-24 15:41:58 +0200

    Implement PublicKey interface in SecP256K1PublicKey class

--- android-client/app/src/main/java/org/hermes/crypto/SecP256K1PubKey.kt
+++ android-client/app/src/main/java/org/hermes/crypto/SecP256K1PubKey.kt
@@ -1,12 +1,14 @@
 package org.hermes.crypto
 
 import java.math.BigInteger
+import java.security.PublicKey
+
 import org.bouncycastle.math.ec.ECPoint
 import org.bouncycastle.math.ec.FixedPointCombMultiplier
 import org.bouncycastle.util.encoders.Hex
 
 
-open class SecP256K1PubKey(val x: BigInteger, val y: BigInteger) {
+open class SecP256K1PubKey(val x: BigInteger, val y: BigInteger): PublicKey {
 
     companion object {
         fun fromBitcoinPubKey(pubKey: String): SecP256K1PubKey {
@@ -23,21 +25,6 @@ open class SecP256K1PubKey(val x: BigInteger, val y: BigInteger) {
     // Store public key in a form that is compatible with the Bouncy Castle internal data structures
     val bcPoint: ECPoint = Secp256K1Curve.ecDomainParameters.curve.createPoint(x, y)
 
-    // This field stores the public key in a standard encoded form:
-    // one byte at the beginning (0x04), the affine x coordinate as a big endian integer byte array
-    // and the affine y coordinate as a big endian integer byte array.
-    open val encoded: ByteArray by lazy {
-        val xB = x.toByteArray()
-        val yB = y.toByteArray()
-        val xOffset = if (xB[0] == 0.toByte()) 1 else 0
-        val yOffset = if (yB[0] == 0.toByte()) 1 else 0
-        val encodedByteArray = ByteArray(x.toByteArray().size + y.toByteArray().size + 1 - xOffset - yOffset)
-        encodedByteArray[0] = 4.toByte()
-        System.arraycopy(xB, 0 + xOffset, encodedByteArray, 1, xB.size - xOffset)
-        System.arraycopy(yB, 0 + yOffset, encodedByteArray, 1 + xB.size - xOffset, yB.size - yOffset)
-        encodedByteArray
-    }
-
     // The encoded field as a Hex string
     val encodedHex: String by lazy { Hex.toHexString(encoded) }
 
@@ -53,4 +40,26 @@ open class SecP256K1PubKey(val x: BigInteger, val y: BigInteger) {
                 privateKey.value)
             .normalize()
         )
+
+    override fun getAlgorithm(): String = "SECP256K1"
+
+    /**
+     * Encodes the public key in a standard encoded form:
+     * one byte at the beginning (0x04), the affine x coordinate as
+     * a big endian integer byte array and the affine y coordinate
+     * as a big endian integer byte array.
+     */
+    override fun getEncoded(): ByteArray {
+        val xB = x.toByteArray()
+        val yB = y.toByteArray()
+        val xOffset = if (xB[0] == 0.toByte()) 1 else 0
+        val yOffset = if (yB[0] == 0.toByte()) 1 else 0
+        val encodedByteArray = ByteArray(x.toByteArray().size + y.toByteArray().size + 1 - xOffset - yOffset)
+        encodedByteArray[0] = 4.toByte()
+        System.arraycopy(xB, 0 + xOffset, encodedByteArray, 1, xB.size - xOffset)
+        System.arraycopy(yB, 0 + yOffset, encodedByteArray, 1 + xB.size - xOffset, yB.size - yOffset)
+        return encodedByteArray
+    }
+
+    override fun getFormat(): String = "ASN.1"
 }
