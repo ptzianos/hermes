62dc910c3d4cf756be8b0764fdd998e916e85a44
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-10-03 13:49:17 +0200

    Specify that type parameter of public key in KeyEncoder is BIP32PubKey

--- android-client/app/src/main/java/org/hermes/hd/BIP32.kt
+++ android-client/app/src/main/java/org/hermes/hd/BIP32.kt
@@ -257,7 +257,7 @@ interface KeyRegistry {
     fun put(path: String, key: BIP32Key)
 }
 
-abstract class KeyEncoder<in PrK : BIP32Key, in PuK> {
+abstract class KeyEncoder<in PrK : BIP32Key, in PuK: BIP32PubKey> {
     abstract fun encodePrivateKey(key: PrK, options: Map<String, Any>): String
 
     abstract fun encodePublicKey(key: PuK, options: Map<String, Any>): String
