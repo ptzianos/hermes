cdfa7b93866dc80942bd734533daaa0a8d5c52b6
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-10-08 00:08:12 +0200

    Add SHA512 version of PBKDF2

--- android-client/app/src/main/java/org/hermes/crypto/algorithms.kt
+++ android-client/app/src/main/java/org/hermes/crypto/algorithms.kt
@@ -3,4 +3,5 @@ package org.hermes.crypto
 object CryptoAlgorithms {
     const val HMAC_SHA512: String = "HmacSHA512"
     const val PBKDF2_HMC_SHA1: String = "PBKDF2WithHmacSHA1"
+    const val PBKDF2_HMC_SHA512: String = "PBKDF2WithHmacSHA512"
 }
