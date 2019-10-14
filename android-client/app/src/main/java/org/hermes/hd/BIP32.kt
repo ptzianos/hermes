33377a472c108a262f9ab710754e56ac3ed96cb9
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-10-14 22:44:35 +0200

    Split CKDPriv function away from child method in ExPrivKey

--- android-client/app/src/main/java/org/hermes/hd/BIP32.kt
+++ android-client/app/src/main/java/org/hermes/hd/BIP32.kt
@@ -151,28 +151,37 @@ class ExPrivKey(
 
     init { BIP32.verify(path) }
 
-    fun CKDPriv(index: Long): Triple<String, ByteArray, BigInteger> {
-        if ((index < 0) or (index >= BIP32.MAX_KEY_INDEX))
-            throw BIP32Key.InvalidIndex()
+    companion object {
+        fun CKDPriv(
+            index: Long,
+            key: ByteArray,
+            public: ByteArray,
+            chainCode: ByteArray
+        ): Pair<ByteArray, BigInteger> {
+            if ((index < 0) or (index >= BIP32.MAX_KEY_INDEX))
+                throw BIP32Key.InvalidIndex()
 
-        val n = Secp256K1Curve.X9ECParameters.n
-        val digest = Mac.getInstance(HMAC_SHA512).apply { init(SecretKeySpec(chainCode, HMAC_SHA512)) }
-        val I = when (index >= HARDENED_KEY_OFFSET) {
-            true -> digest.doFinal(value.toByteArray().extendOrReduceTo(33, padStart = true) +
-                    index.toByteArray().extendOrReduceTo(4, padStart = true))
-            else -> digest.doFinal(public.encoded +
-                    index.toByteArray().extendOrReduceTo(4, padStart = true))
+            val n = Secp256K1Curve.X9ECParameters.n
+            val digest = Mac.getInstance(HMAC_SHA512).apply { init(SecretKeySpec(chainCode, HMAC_SHA512)) }
+            val I = when (index >= HARDENED_KEY_OFFSET) {
+                true -> digest.doFinal(
+                    key.extendOrReduceTo(33, padStart = true) +
+                            index.toByteArray().extendOrReduceTo(4, padStart = true)
+                )
+                else -> digest.doFinal(
+                    public + index.toByteArray().extendOrReduceTo(4, padStart = true)
+                )
+            }
+            return Pair(
+                I.sliceArray(32 until 64),
+                (I.sliceArray(0 until 32).toBigInt(positive = true) + BigInteger(key)).mod(n)
+            )
         }
-        return Triple(
-            BIP32Key.pathOfChild(path, index),
-            I.sliceArray(32 until 64),
-            (I.sliceArray(0 until 32).toBigInt(positive = true) + value).mod(n)
-        )
     }
 
     override fun child(index: Long): BIP32Key {
-        val (childPath, chainCode, key) = CKDPriv(index)
-        return ExPrivKey(childPath, this, chainCode, key)
+        val (chainCode, key) = CKDPriv(index, value.toByteArray(), public.encoded, chainCode)
+        return ExPrivKey(BIP32Key.pathOfChild(path, index), this, chainCode, key)
     }
 
     override val public: ExPubKey by lazy { ExPubKey(parent?.public, chainCode, path, this, encoder) }
