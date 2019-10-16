1e5e622f8388ef89bb7e9b242859e573a7cab334
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-10-16 23:56:28 +0200

    Add module and tests for BIP44 wallets

---
+++ android-client/app/src/test/java/org/hermes/hd/BIP44WalletTest.kt
@@ -0,0 +1,26 @@
+package org.hermes.hd
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class BIP44WalletTest {
+
+    @Test
+    fun getWallet() {
+        val words = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about".split(' ')
+        val wallet = BIP44Wallet(seed = Mnemonic.fromWordList(words.toTypedArray()).seed)
+        val btcWallet = wallet.getWalletForCoin("BTC", 0, 0)
+        val ethWallet = wallet.getWalletForCoin("ETH", 0, 0)
+        val iotaWallet = wallet.getWalletForCoin("IOTA", 0, 0)
+        assertTrue(wallet.masterWallet["m"] is ExPrivKey)
+        assertTrue(wallet.masterWallet["m/44'"] is ExPrivKey)
+        assertTrue(wallet.masterWallet["m/44'/${SLIP44.bySymbol["BTC"]!!.walletIndex}/0'/0"] is ExPrivKey)
+        assertTrue(wallet.masterWallet["m/44'/${SLIP44.bySymbol["ETH"]!!.walletIndex}/0'/0"] is ExPrivKey)
+        assertTrue(wallet.masterWallet["m/44'/${SLIP44.bySymbol["IOTA"]!!.walletIndex}/0'/0"] is IOTAExPrivKey)
+        assertTrue(iotaWallet[0] is IOTAExPrivKey)
+        assertArrayEquals(
+            (iotaWallet[0] as IOTAExPrivKey).value.toByteArray(),
+            (wallet.masterWallet["m/44'/${SLIP44.bySymbol["IOTA"]!!.walletIndex}/0'/0/0"] as IOTAExPrivKey).value.toByteArray()
+        )
+    }
+}
