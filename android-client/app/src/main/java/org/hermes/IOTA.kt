b7edf4e00200d6d6b1a5720461edfcd12e0cc6b4
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2019-11-17 23:52:45 +0100

    Update Android client utils

--- android-client/app/src/main/java/org/hermes/IOTA.kt
+++ android-client/app/src/main/java/org/hermes/IOTA.kt
@@ -1,10 +1,7 @@
-package org.hermes.iota
+package org.hermes
 
 object IOTA {
-
     object Transaction {
-
         const val SIGNATURE_MESSAGE_FRAGMENT: Int = 2187
-
     }
 }
--- wallet/extensions/src/main/kotlin/org/hermes/extensions/intarray.kt
+++
@@ -1,6 +0,0 @@
-package org.hermes.extensions
-
-import org.hermes.ternary.Trit
-import org.hermes.ternary.TritArray
-
-fun IntArray.toTritArray(): TritArray = TritArray(this.size) { Trit(this[it]) }
